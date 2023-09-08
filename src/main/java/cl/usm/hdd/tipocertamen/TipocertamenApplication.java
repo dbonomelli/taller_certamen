e1258d08927b40391e5075018f5cfac2df6a7831
unknown <diego.munozde@sansano.usm.cl>
2023-09-08 15:03:44 -0300

    initial push

---
+++ src/main/java/cl/usm/hdd/tipocertamen/TipocertamenApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.tipocertamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipocertamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipocertamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usm/hdd/tipocertamen/TipocertamenApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.hdd.tipocertamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipocertamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
